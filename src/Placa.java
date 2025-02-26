e3c067f35bcf94955529e220297f82d3e4963732
Cerry <denis.carreto@hotmail.com>
2025-02-25 23:05:56 -0300

    Coleção de dados 1

---
+++ src/Placa.java
@@ -0,0 +1,70 @@
+public class Placa {
+    private char l1, l2, l3;
+    private char n1, n2, n3, n4;
+
+    Placa(char l1, char l2, char l3, char n1, char n2, char n3, char n4) {
+        l1 = this.l1;
+        l2 = this.l2;
+        l3 = this.l3;
+        n1 = this.n1;
+        n2 = this.n2;
+        n3 = this.n3;
+        n4 = this.n4;
+    }
+
+    public char getL1() {
+        return l1;
+    }
+
+    public void setL1(char l1) {
+        this.l1 = l1;
+    }
+
+    public char getL2() {
+        return l2;
+    }
+
+    public void setL2(char l2) {
+        this.l2 = l2;
+    }
+
+    public char getL3() {
+        return l3;
+    }
+
+    public void setL3(char l3) {
+        this.l3 = l3;
+    }
+
+    public char getN1() {
+        return n1;
+    }
+
+    public void setN1(char n1) {
+        this.n1 = n1;
+    }
+
+    public char getN2() {
+        return n2;
+    }
+
+    public void setN2(char n2) {
+        this.n2 = n2;
+    }
+
+    public char getN3() {
+        return n3;
+    }
+
+    public void setN3(char n3) {
+        this.n3 = n3;
+    }
+
+    public char getN4() {
+        return n4;
+    }
+
+    public void setN4(char n4) {
+        this.n4 = n4;
+    }
+}

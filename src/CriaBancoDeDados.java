a3e57225a188046e4b8fe668972888356828a0a5
Cerry <denis.carreto@hotmail.com>
2025-03-10 11:00:06 -0300

    - Refatoração - Divisão de responsabilidades

---
+++ src/CriaBancoDeDados.java
@@ -0,0 +1,85 @@
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class CriaBancoDeDados {
+    char l1, l2, l3;
+    int n1, n2, n3, n4;
+    char ms1, ms2, ms3, ms4;
+    int msn1, msn2, msn3;
+    long contadorBR = 0;
+    long contadorMS = 0;
+
+    void bancoDeDados() {
+
+        String pastaDoBanco = "Banco de Dados";
+        String caminhoDados = "Banco de Dados/";
+
+        File pasta = new File(pastaDoBanco);
+
+        if(!pasta.exists()) {
+            boolean criada = pasta.mkdir();
+
+            if(criada) {
+                System.out.println("Pasta do banco de dados foi criada!");
+            } else {
+                System.out.println("Falha ao criar a pasta do banco.");
+            }
+        } else {
+            System.out.println("A pasta ja existe!");
+        }
+
+        for (l1 = 'A'; l1 <= 'Z'; l1++) {
+            System.out.println("Criando PlacaBr com inicial " + l1 + ".txt");
+            String nomeArquivo = caminhoDados + "PlacaBr com inicial " + l1 + ".txt";
+            try (BufferedWriter writer = new BufferedWriter(new FileWriter(nomeArquivo))) {
+                for (l2 = 'A'; l2 <= 'Z'; l2++) {
+                    for (l3 = 'A'; l3 <= 'Z'; l3++) {
+                        for (n1 = 0; n1 <= 9; n1++) {
+                            for (n2 = 0; n2 <= 9; n2++) {
+                                for (n3 = 0; n3 <= 9; n3++) {
+                                    for (n4 = 0; n4 <= 9; n4++) {
+                                        String placaBR = String.format("%c%c%c-%d%d%d%d", l1, l2, l3, n1, n2, n3, n4);
+                                        writer.write(placaBR + "\t");
+                                        contadorBR++;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            } catch (IOException exc) {
+                System.out.println("Erro ao escrever lno arquivo: " + nomeArquivo);
+                exc.printStackTrace();
+            }
+            System.out.println("Banco de dados: PlacaBr com inicial " + l1 + " criado!");
+        }
+        for (ms1 = 'A'; ms1 <= 'Z'; ms1++) {
+            System.out.println("Criando Placa Mercosul com inicial " + ms1 + ".txt");
+            String nomeArquivoMS = caminhoDados + "PlacasMercosul com inicial " + ms1 + ".txt";
+            try (BufferedWriter writer = new BufferedWriter(new FileWriter(nomeArquivoMS))) {
+                for (ms2 = 'A'; ms2 <= 'Z'; ms2++) {
+                    for (ms3 = 'A'; ms3 <= 'Z'; ms3++) {
+                        for (msn1 = 0; msn1 <= 9; msn1++) {
+                            for (ms4 = 'A'; ms4 <= 'Z'; ms4++) {
+                                for (msn2 = 0; msn2 <= 9; msn2++) {
+                                    for (msn3 = 0; msn3 <= 9; msn3++) {
+                                        String placaMS = String.format("%c%c%c%d%c%d%d", ms1, ms2, ms3, msn1, ms4, msn2, msn3);
+                                        writer.write(placaMS + "\t");
+                                        contadorMS++;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            } catch (IOException e) {
+                System.out.println("Erro ao escrever no arquivo: " + nomeArquivoMS);
+                e.printStackTrace();
+            }
+            System.out.println("Banco de dados: Placa Mercosul com inicial " + ms1 + " criado!");
+        }
+        System.out.println("Banco de dados criado!");
+    }
+}
--- src/Placa.java
+++ src/Placa.java
@@ -6,66 +6,8 @@ import java.io.IOException;
 public class Placa {
     public static void main(String[] args) {
 
-        char l1, l2, l3;
-        int n1, n2, n3, n4;
-        char ms1, ms2, ms3, ms4;
-        int msn1, msn2, msn3;
-        long contadorBR = 0;
-        long contadorMS = 0;
+        CriaBancoDeDados criar = new CriaBancoDeDados();
 
-        for (l1 = 'A'; l1 <= 'Z'; l1++) {
-            String nomeArquivo = "PlacaBr com inicial " + l1 + ".txt"; // tentar função
-            try (BufferedWriter writer = new BufferedWriter(new FileWriter(nomeArquivo))) {
-                for (l2 = 'A'; l2 <= 'Z'; l2++) {
-                    for (l3 = 'A'; l3 <= 'Z'; l3++) {
-                        for (n1 = 0; n1 <= 9; n1++) {
-                            for (n2 = 0; n2 <= 9; n2++) {
-                                for (n3 = 0; n3 <= 9; n3++) {
-                                    for (n4 = 0; n4 <= 9; n4++) {
-                                        String placaBR = String.format("%c%c%c-%d%d%d%d", l1, l2, l3, n1, n2, n3, n4);
-                                        // System.out.print(placaBR + "\t");
-                                        writer.write(placaBR + "\t");
-                                        contadorBR++;
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            } catch (IOException exc) {
-                System.out.println("Erro ao escrever lno arquivo: " + nomeArquivo);
-                exc.printStackTrace();
-            }
-        }
-        for (ms1 = 'A'; ms1 <= 'Z'; ms1++) {
-            String nomeArquivoMS = "PlacasMercosul com inicial " + ms1 + ".txt";
-            try (BufferedWriter writer = new BufferedWriter(new FileWriter(nomeArquivoMS))) {
-                for (ms2 = 'A'; ms2 <= 'Z'; ms2++) {
-                    for (ms3 = 'A'; ms3 <= 'Z'; ms3++) {
-                        for (msn1 = 0; msn1 <= 9; msn1++) {
-                            for (ms4 = 'A'; ms4 <= 'Z'; ms4++) {
-                                for (msn2 = 0; msn2 <= 9; msn2++) {
-                                    for (msn3 = 0; msn3 <= 9; msn3++) {
-                                        String placaMS = String.format("%c%c%c%d%c%d%d", ms1, ms2, ms3, msn1, ms4, msn2, msn3);
-                                        writer.write(placaMS + "\t");
-                                        contadorMS++;
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            } catch (IOException e) {
-                System.out.println("Erro ao escrever no arquivo: " + nomeArquivoMS);
-                e.printStackTrace();
-            }
-        }
-        System.out.println("Contador final é de: " + contadorBR + " combinações para as placas padrão BR.");
-        System.out.println("Contador final é de: " + contadorMS + " combinações para as placas padrão Mercosul.");
-        System.out.println("Total de combinações possíveis entre placas padrão BR e Mercosul é de " + (contadorBR + contadorMS));
+        criar.bancoDeDados();
     }
 }
-/*
-todo
-    - função para tornar o código mais resumido.
-*/
